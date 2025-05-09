e3d96bcdfdc1b10d66ad9625175b733ef43aac5f
medhaviSharma11apr <medhavisharma11apr@gmail.com>
2025-05-09 16:12:48 +0530

    new commit

--- lib/data/repository/todo_repository.dart
+++ lib/data/repository/todo_repository.dart
@@ -6,6 +6,7 @@ import 'package:fpdart/fpdart.dart';
 import 'package:todoflutterapp/core/error/failure.dart';
 import 'package:todoflutterapp/core/error/server_exception.dart';
 import 'package:todoflutterapp/core/utils/appwrite_constants.dart';
+import 'package:todoflutterapp/data/model/todo_model.dart';
 import 'package:todoflutterapp/data/provider/appwrite_provider.dart';
 
 import '../../core/locator/locator.dart';
@@ -16,6 +17,9 @@ abstract class ITodoRepository {
     required String title,
     required String description,
     required bool isCompleted,
+  });
+  Future<Either<Failure, List<TodoModel>>> getTodo({
+    required String userId,
   });
   // Future<int> insertTodo(Map<String, dynamic> todo);
   // Future<int> updateTodo(Map<String, dynamic> todo);
@@ -46,6 +50,28 @@ class TodoRepository implements ITodoRepository {
           });
       log('doc${document.data.toString()}');
       return right(document);
+    } on AppwriteException catch (e) {
+      return left(Failure(e.message ?? e.toString()));
+    } on ServerException catch (e) {
+      return left(Failure(e.message ?? e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<TodoModel>>> getTodo(
+      {required String userId}) async {
+    try {
+      DocumentList list = await _appWriteProvider.databases!.listDocuments(
+        databaseId: AppWriteConstants.databaseId,
+        collectionId: AppWriteConstants.todoCollectionId,
+        queries: [Query.equal('userId', userId)],
+      );
+      Map<String, dynamic> data = list.toMap();
+      List listdata = data['documents'].toList();
+
+      List<TodoModel> todoList =
+          listdata.map((e) => TodoModel.fromMap(e['data'])).toList();
+      return right(todoList);
     } on AppwriteException catch (e) {
       return left(Failure(e.message ?? e.toString()));
     } on ServerException catch (e) {
